af895b159906546670497ba23fa7f18bec39da76
Oludairo <eoludairo61@gmail.com>
2021-10-01 17:50:49 +0100

    ...

--- src/models/School.java
+++ src/models/School.java
@@ -1,4 +1,16 @@
 package models;
 
+import java.util.ArrayList;
+
 public class School {
+    private long id;
+    private String name;
+    private String description;
+    private ArrayList<Class> classes = new ArrayList<>();
+    private SchoolType type;
+    private ArrayList<Staff> staffs = new ArrayList<>();
+    private Address address;
+    private ArrayList<Student> students = new ArrayList<>();
+
+
 }
